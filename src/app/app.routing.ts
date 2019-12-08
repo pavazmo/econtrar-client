564357d9f8ef3652760b5c9f7fffa05ac4b69d2b
pavazmo <pavazmo@gmail.com>
2019-12-08 17:44:25 +0100

    fixing bugs
    remove landing in app.routing due to error deploying

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ComponentsComponent } from './components/components.component';
 import { ProfileComponent } from './examples/profile/profile.component';
 import { SignupComponent } from './examples/signup/signup.component';
-import { LandingComponent } from './examples/landing/landing.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 
 const routes: Routes =[
@@ -14,7 +13,6 @@ const routes: Routes =[
     { path: 'home',             component: ComponentsComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'signup',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
     { path: 'nucleoicons',      component: NucleoiconsComponent }
 ];
 
